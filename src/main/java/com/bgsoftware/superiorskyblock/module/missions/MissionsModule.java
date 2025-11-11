35a86ccb3f00479040ca084874d37402ca2c0120
airoons <thegamerslvlv@gmail.com>
2025-11-12 00:39:02 +0200

    Removed default mission jar file generating

--- src/main/java/com/bgsoftware/superiorskyblock/module/missions/MissionsModule.java
+++ src/main/java/com/bgsoftware/superiorskyblock/module/missions/MissionsModule.java
@@ -380,16 +380,6 @@ public class MissionsModule extends BuiltinModule<MissionsModule.Configuration> 
     }
 
     private void generateDefaultMissionJars() {
-        Resources.copyResource("modules/missions/BlocksMissions");
-        Resources.copyResource("modules/missions/BrewingMissions");
-        Resources.copyResource("modules/missions/CraftingMissions");
-        Resources.copyResource("modules/missions/EnchantingMissions");
-        Resources.copyResource("modules/missions/FarmingMissions");
-        Resources.copyResource("modules/missions/FishingMissions");
-        Resources.copyResource("modules/missions/IslandMissions");
-        Resources.copyResource("modules/missions/ItemsMissions");
-        Resources.copyResource("modules/missions/KillsMissions");
-        Resources.copyResource("modules/missions/StatisticsMissions");
     }
 
     private static class MissionsComparator implements Comparator<Mission<?>> {
